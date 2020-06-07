a9c56b189219d5ac310b24a61a142aa880470efc
Paweł Górski <dev.baymax42@gmail.com>
2020-06-07 18:30:13 +0200

    Added base for project context unit tests

--- apps/rex_data/test/support/factory.ex
+++ apps/rex_data/test/support/factory.ex
@@ -4,16 +4,29 @@ defmodule RexData.Factory do
   @typedoc """
   Alias for the all used factory atoms used in this module.
   """
-  @type valid_factories :: :node
+  @type valid_factories :: :node | :project
 
   @default_node_id "b9bbbbc8-a8b7-11ea-bb37-0242ac130002"
   @default_node_name "TEST_NODE"
+  @project_id 1
 
   @spec build(:node) :: RexData.Worker.Node.t()
   def build(:node),
     do: %RexData.Worker.Node{
       node_id: @default_node_id,
       name: @default_node_name
+    }
+
+  @spec build(:project) :: RexData.Project.ProjectInfo.t()
+  def build(:project),
+    do: %RexData.Project.ProjectInfo{
+      id: @project_id,
+      path: "path",
+      type: :animation,
+      width: 480,
+      height: 360,
+      starting_frame: 5,
+      total_frames: 10
     }
 
   @spec build(valid_factories, []) :: any
